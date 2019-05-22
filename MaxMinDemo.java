23315e074df443e60ee8e728a20e4d212d9bef48
sabbir-90 <48676253+sabbir-90@users.noreply.github.com>
2019-05-22 16:29:02 +0600

    Add files via upload

---
+++ MaxMinDemo.java
@@ -0,0 +1,32 @@
+package evidencepreparation;
+
+import java.util.Scanner;
+
+public class MaxMinDemo {
+
+//    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//        int[] inputs = new int[3];
+//        int max = 0;
+//        int min = 0;
+//        for (int i = 0; i < inputs.length; i++) {
+//            System.out.println("Enter number");
+//            inputs[i] = input.nextInt();
+//
+//            if (i == 0) {
+//                max = inputs[0];
+//                min = inputs[0];
+//            }
+//
+//            if (max < inputs[i]) {
+//                max = inputs[i];
+//            }
+//            if (min > inputs[i]) {
+//                min = inputs[i];
+//            }
+//        }
+//        System.out.println(" Maximum number: " + max);
+//        System.out.println(" Minimum number is " + min);
+//    }
+
+}
