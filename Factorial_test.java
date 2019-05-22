e8ab7a1abd04b0bf712f038802f1f80712ae5693
Sabbir Hossain <sabbirhossain9901@gmail.com>
2019-05-22 15:31:32 +0600

    Factorial change

--- Factorial_test.java
+++ Factorial_test.java
@@ -8,15 +8,13 @@ public class Factorial_test {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter number");
-        int n = input.nextInt();
-        System.out.println("factorial "+n+" value "+fac(n) );
-       
+        int num = input.nextInt();
+        int fact =1; 
+	
+	for(int i= num; i>=1; i--){
+	fact = fact * i; 
+	}
+       System.out.print("Factorial of "+ num+ "="+ fact);
     }
-    public static double fac(int n){
-        if(n==0)
-            return 1;
-        
-        else
-        return n * fac(n-1);
-    }
+
 }
